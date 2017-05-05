74d46abae899ef13a6044243d817783f5e4a4bd2
Mtaandao <mtaandao@gmail.com>
2017-05-05 11:08:12 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'functions/jabali.php';
+
+$hUser = new _hUsers;
+$hResource = new _hResources;
+$hService = new _hServices;
+$hMessage = new _hMessages;
+$hNote = new _hNotes;
+$hForm = new _hForms;
+
+if(isset($_GET['login'])) {
+  $hForm -> loginForm();
+} elseif(isset($_GET['confirm'])) {
+  $hForm -> loginForm();
+} elseif(isset($_GET['forgot'])) {
+  $hForm -> loginForm();
+} elseif(isset($_GET['reset'])) {
+  $hForm -> loginForm();
+} elseif(isset($_GET['logout'])) {
+  $hForm -> loginForm();
+} else {
+  $hForm -> startForm();
+}
+
+if(isset($_POST['login'])) {
+  $hUser -> loginUser();
+}
