e0e8f036846da8d34a68156c4eb7bdd7d905b3c0
Mtaandao <mtaandao@gmail.com>
2017-05-05 11:26:25 +0300

    Create users.php

---
+++ functions/users.php
@@ -0,0 +1,10 @@
+<?php
+
+class _hUsers() {
+h_alias, h_author, h_avatar, h_center, h_code, h_created, h_custom, h_description, h_email, h_key, h_level, h_link, h_location, h_logdate, h_logip, h_notes, h_phone, h_status, h_style, h_type, h_updated
+h_alias, h_author, h_avatar, h_center, h_code, h_created, h_custom, h_description, h_email, h_key, h_level, h_link, h_location, h_logdate, h_logip, h_notes, h_phone, h_status, h_style, h_type, $h_updated
+  var h_h_alias, h_author, h_avatar, h_center, h_code, h_created, h_custom, h_description, h_email, h_key, h_level, h_link, h_location, h_logdate, h_logip, h_notes, h_phone, h_status, h_style, h_type, h_updated
+  
+  function createUser() {
+  }
+}
