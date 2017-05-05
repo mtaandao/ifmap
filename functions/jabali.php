f878caee7a48b4ec1060b922883a8c0b2302fdae
Mtaandao <mtaandao@gmail.com>
2017-05-05 11:38:34 +0300

    Create jabali.php

---
+++ functions/jabali.php
@@ -0,0 +1,22 @@
+<?php
+//Main Functions file
+
+//Database Constants
+define("hDBNAME","");
+define("hDBNAME","");
+define("hDBNAME","");
+define("hDBNAME","");
+
+//Directories
+define("hDBNAME","");
+
+function connDatabase() {
+}
+function getFile($path, $file) {
+}
+
+function getStyle() {
+}
+
+function getScript() {
+}
